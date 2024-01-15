7ac6ecc4fc700508e8ceb858f93497b1a9517837
Mohammed Atef <mohammed.atef.ewais@gmail.com>
2024-01-15 05:15:51 +0200

    ready template to start react projects

--- src/store/Store.js
+++ src/store/Store.js
@@ -1,10 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import ThemeReducer from "./Reducers/ThemeReducer";
 
+const dev_env = process.env.REACT_APP_ENV === "dev";
+
 export const store = configureStore({
   reducer: {
     theme: ThemeReducer,
   },
+  devTools: dev_env,
 });
 
 export const AppDispatch = typeof store.dispatch;
